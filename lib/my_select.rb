a0bfaaba4a29994fe0854f47e6aed598d29f6510
derek chen <derek.g.chen@gmail.com>
2018-03-22 17:39:32 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,13 @@
 def my_select(collection)
  # your code here!
  i = 0
+ temp = []
+ temp1 = []
  while(i < collection.size) do
-   yield(collection[i])
+   temp = yield(collection[i])
+   temp1 << temp
    i+=1
  end
 
- 
+
 end
